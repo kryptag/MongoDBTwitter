79b26599ccede3a5ddfd79aaafe35160b9e487fe
Florent Haxha <kryptag@gmail.com>
2018-02-11 19:00:28 +0100

    changes to how you run the file and fixed the POM

--- src/main/java/com/mycompany/mongodbtwitter/Main.java
+++ src/main/java/com/mycompany/mongodbtwitter/Main.java
@@ -1,37 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.mongodbtwitter;
-
-
 
 /**
  *
  * @author florenthaxha
  */
 public class Main {
+        
     public static void main(String[] args) {
         DBLogic dbl = new DBLogic();
         if(null != args[0])switch (args[0]) {
             case "getTotalUsers":
-                System.out.println("Total amount of users: " + dbl.getTotalUsers());
+                System.out.println("");
+                System.out.println("\n" + "Total amount of users: " + dbl.getTotalUsers());
                 break;
             case "getTop10Mentioners":
-                System.out.println("Top 10 Mentioners: " + dbl.getTop10Mentioners());
+                System.out.println("\n" + "Top 10 Mentioners: " + dbl.getTop10Mentioners());
                 break;
             case "getTop5Mentioned":
-                System.out.println(dbl.getTop5Mentioned());
+                System.out.println("\n" + "Top 5 Mentioned users: "+dbl.getTop5Mentioned());
                 break;
             case "getTop10MostActive":
-                System.out.println(dbl.getTop10MostActive());
+                System.out.println("\n" + "Top 10 most active users" + dbl.getTop10MostActive());
                 break;
             case "getTop5Happy":
-                System.out.println(dbl.getTop5Happy());
+                System.out.println("\n" + "Top 5 happy users" + dbl.getTop5Happy());
                 break;
             case "getTop5Grumpy":
-                System.out.println(dbl.getTop5Grumpy());
+                System.out.println("\n" + "Top 5 grumpy users" + dbl.getTop5Grumpy());
                 break;
             default:
                 break;
